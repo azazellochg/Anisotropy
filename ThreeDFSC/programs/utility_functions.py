18ed865f401c3faaad8676777a09c398d0faca15
Grigory Sharov <sharov.grigory@gmail.com>
2020-04-16 23:08:30 +0100

    clean up & testing 1

--- ThreeDFSC/programs/utility_functions.py
+++ ThreeDFSC/programs/utility_functions.py
@@ -14,12 +14,12 @@ def print_progress(iteration, total, prefix='', suffix='', decimals=1):
         bar_length    - Optional    : character length of bar (Int)
     """
 
-    #rows, columns = os.popen('stty size', 'r').read().split()
+    # rows, columns = os.popen('stty size', 'r').read().split()
     columns = 40
-    bar_length = int(float(columns)/2)
+    bar_length = int(float(columns) / 2)
     str_format = "{0:." + str(decimals) + "f}"
     percents = str_format.format(100 * (iteration / float(total)))
-    filled_length = int(round(bar_length * iteration / float(total))) ## adjusted base on window size
+    filled_length = int(round(bar_length * iteration / float(total)))  # adjusted base on window size
     bar = '*' * filled_length + '-' * (bar_length - filled_length)
 
     sys.stdout.write('\x1b[2K\r%s |%s| %s%s %s' % (prefix, bar, percents, '%', suffix)),
@@ -31,11 +31,11 @@ def print_progress(iteration, total, prefix='', suffix='', decimals=1):
 
 ## Added to mute the print statements
 def blockPrint():
-        sys.stdout = open(os.devnull, 'w')
-        pass
+    sys.stdout = open(os.devnull, 'w')
+    pass
+
 
 def enablePrint():
-#       sys.stdout = open('threedfscstdout.log', 'a')
-        #sys.stdout = sys.__stdout__
-        pass
-
+    #       sys.stdout = open('threedfscstdout.log', 'a')
+    # sys.stdout = sys.__stdout__
+    pass
